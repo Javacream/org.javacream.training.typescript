e81b58ca079f51e39fc7ad0f7180dd0a66481e88
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-01-16 16:21:44 +0100

    add promise and async_await

---
+++ ts/async_await.ts
@@ -0,0 +1,24 @@
+namespace async_await{
+async function firstAsyncFunction(delay:number): Promise<string> {
+  console.log("Entering firstAsyncFunction at " + new Date())
+    var result = new Promise((resolve) => {
+        setTimeout(() => resolve("World"), delay);
+    });
+    return result;
+}
+async function secondAsyncFunction(param:string, delay:number): Promise<string> {
+    console.log("Entering secondAsyncFunction at " + new Date())
+    var result = new Promise((resolve) => {
+        setTimeout(() => resolve(`Hello ${param}`), delay);
+    });
+    return result;
+}
+async function sequence(){
+  console.log("Entering sequence at " + new Date())
+  var result = await firstAsyncFunction(1000)
+  var result2 = await secondAsyncFunction(result, 2000)
+  console.log(`result: ${result}, result2: ${result2} at ${new Date()}`)
+}
+
+sequence()
+}
