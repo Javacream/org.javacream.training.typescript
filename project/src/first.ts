7f812a8323c52c5530bea366e9b0e6093804a22f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-01-23 07:24:48 +0100

    setup project

---
+++ project/src/first.ts
@@ -0,0 +1,3 @@
+export let HUGO = "hugo"
+
+console.log("HUHU")
---
+++ project/test/first.test.ts
@@ -0,0 +1,12 @@
+let first = require('../src/first')
+
+test('first', () => {
+    expect('ts')
+      .toBe('ts');
+  });
+
+
+test('first', () => {
+  expect(first.HUGO)
+    .toBe('hugo');
+});
