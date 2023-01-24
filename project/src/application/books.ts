867ddf44b5f9b1d82d8f28e4485a19bdc0d863c7
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-01-24 10:12:58 +0100

    house keeping...

--- project/src/application/books.ts
+++ project/src/application/books.ts
@@ -1,17 +1,6 @@
 const applicationName = "BOOKS APPLICATION"
 
-function doDemo(){
-
-}
-
-//Syntactic sugar:
 module.exports = {
     applicationName, 
-    doDemo
 }
-
-// module.exports = {
-//     hugo: applicationName,
-//     emil: doDemo
-// }
 
