f9f912c75e35c095e1ca8b9fcdf7bf686b05c218
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-01-25 15:04:44 +0100

    introduce file system and application

--- project/index.ts
+++ project/index.ts
@@ -1,5 +1,18 @@
-function main(){
+import {readFile, writeFile} from "fs/promises"
+import { StoreController } from "./src/application/storeservice"
+async function main(){
+    let storeController = new StoreController()
     console.log(`starting main... `)
+    let ids = await readFile('./data/in/ids.txt', 'utf-8')
+    let idList = ids.split(",")
+    let stockList = new Map<string, number>()
+
+    for (let id of idList){
+        let stock = await storeController.getStock("sawitzki", id)
+        stockList.set(id, stock)
+    }
+    console.log(stockList)
+    await writeFile('./data/out/stocks.txt', JSON.stringify(stockList))
     console.log("done")
 }
 
